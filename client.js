f53954ce6fa70a7fe5a48d4b60cbf4569ddb4dd1
Ian Lambert <lambe347@umn.edu>
2021-04-12 23:59:23 -0500

    base functionality done, need to style and add comments done. Stretch goals?

--- client.js
+++ client.js
@@ -3,44 +3,43 @@ console.log('Here are all the available people:', people);
 $(document).ready(onReady);
 let min = 1;
 let max = 7;
-
-
-
+let person = '';
+let userName = '';
 function onReady(){
-    
-    $('#addHere').append(
-        `<img src="https://github.com/devjanaprime.png?size=250" alt="Profile image of Dev"></img>`
-    );
-    $('#addHere').append(
-        `<img src="https://github.com/DoctorHowser.png?size=250" alt="Profile image of Dane"></img>`
-    );
-    $('#addHere').append(
-        `<img src="https://github.com/mbmosman.png?size=250" alt="Profile image of Mary"></img>`
-    );
-    $('#addHere').append(
-        `<img src="https://github.com/kdszafranski.png?size=250" alt="Profile image of Kris"></img>`
-    );
-    $('#addHere').append(
-        `<img src="https://github.com/eschwartz.png?size=250" alt="Profile image of Edan"></img>`
-    );
-    $('#addHere').append(
-        `<img src="https://github.com/cassandradanger.png?size=250" alt="Profile image of Casie"></img>`
-    );
-    $('#addHere').append(
-        `<img src="https://github.com/christopher-black.png?size=250" alt="Profile image of Chris"></img>`
-    );
+
+
+    for (let i = 0; i < people.length; i++){
+        userName = people[i].githubUsername;
+        let name = people[i].name; 
+        $('#addHere').append(`
+        <div class="img">
+        <img src="https://github.com/${userName}.png?size=250" img class="image" img id="${name}" alt="Profile image of Dev"></img>`)
+    }
+
     randomPerson(min, max);
+    $('#addHere').on('click', '.image', function() {
+        let personID = $(this).attr('id');
+        if (personID === person){
+            alert('YOU WIN!');
+            randomPerson(min, max);
+        }
+        else {
+            alert('Do you know your instructors? Try again!')
+        }
+    });
 }
 
 function randomPerson(min, max) {
-    let el = $('#select');
-    el.empty();
     let number = Math.floor(Math.random() * (1 + max - min) + min);
     console.log(number);
+    let el = $('#select');
+    el.empty();
     for (let i = 0; i < people.length; i++){
         if (i === number - 1){
             el.append(people[i].name);
+            person = people[i].name
         }
     }
+    console.log(person);
+    return person;
 }
-
